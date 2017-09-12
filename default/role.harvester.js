1fe3ccf0d21934e676f0a6769e0dd4d464ce16a7
Markus Pöschl <poeschl_markus@web.de>
2017-09-12 23:22:38 +0200

    Get closes source for harvester

--- default/role.harvester.js
+++ default/role.harvester.js
@@ -5,9 +5,9 @@ let roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            let nearSource = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+            if(creep.harvest(nearSource) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(nearSource);
             }
         }
         else {
